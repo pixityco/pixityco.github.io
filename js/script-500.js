defcfd992f825b62ee9d28fda17e408969b1f8b6
Loryn Chen <loryn.chen@gmail.com>
2018-01-05 17:09:29 -0500

    Test update

---
+++ js/script-500.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('bm-500'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '../json/500.json'
+})
